ff0132adc9f8100775b341814f50934ca705b460
Kippiii <ianorzel@gmail.com>
2023-01-15 12:48:01 -0500

    Adding to Lexical Analysis

--- src/LexicalAnalysis/main/Scan.java
+++ src/LexicalAnalysis/main/Scan.java
@@ -1,11 +1,12 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Scan {
 
     public static void main(String[] args) throws FileNotFoundException {
-        final String filename = "test.java";
-        final boolean trace = false;
+        final String filename = "Factorial.java";
+        final boolean trace = true;
 
         final MiniJavaParser lexer = new MiniJavaParser (new FileInputStream(filename));
         if (trace)
@@ -13,7 +14,17 @@ public class Scan {
         else
             lexer.disable_tracing();
 
-        // TODO
+        ArrayList<Token> tokens = new ArrayList<Token>();
+        while (true) {
+            try {
+                final Token token = lexer.getNextToken();
+                tokens.add(token);
+                if (token.kind == MiniJavaParserConstants.EOF)
+                    break;
+            } catch (TokenMgrError e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
 }
