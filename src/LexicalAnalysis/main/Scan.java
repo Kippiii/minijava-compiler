53fb516383545ff0fdf663257680df5e2481cdf9
Kippiii <ianorzel@gmail.com>
2023-01-16 14:18:07 -0500

    Updating the Makefile as well as adding custom error messages

--- src/LexicalAnalysis/main/Scan.java
+++ src/LexicalAnalysis/main/Scan.java
@@ -1,30 +1,48 @@
+package main;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import parser.*;
 
 public class Scan {
+    static String filename = "Factorial.java";
 
     public static void main(String[] args) throws FileNotFoundException {
-        final String filename = "Factorial.java";
-        final boolean trace = true;
+        if (args.length < 1)
+            filename = "Factorial.java";
+        else
+            filename = args[0];
 
         final MiniJavaParser lexer = new MiniJavaParser (new FileInputStream(filename));
-        if (trace)
-            lexer.enable_tracing();
-        else
-            lexer.disable_tracing();
 
-        ArrayList<Token> tokens = new ArrayList<Token>();
+        int errors = 0;
         while (true) {
             try {
                 final Token token = lexer.getNextToken();
-                tokens.add(token);
                 if (token.kind == MiniJavaParserConstants.EOF)
                     break;
             } catch (TokenMgrError e) {
-                System.out.println(e.getMessage());
+                System.err.println(parseError(e.getMessage()));
+                errors++;
             }
         }
+
+        System.out.printf("filename=%s, errors=%d%n", filename, errors);
+    }
+
+    static String ERROR_RE = "^Lexical error at line (\\d+), column (\\d+).  Encountered: '\\d+' \\(\\d+\\), after prefix \"(.)\"$";
+    static String parseError(String errorMsg) {
+        Pattern r = Pattern.compile(ERROR_RE);
+        Matcher m = r.matcher(errorMsg);
+        m.find();
+        int lineNum = Integer.parseInt(m.group(1));
+        int colNum = Integer.parseInt(m.group(2));
+        char curChar = m.group(3).charAt(0);
+        return String.format("%s:%03d.%03d: ERROR -- illegal character %c", filename, lineNum, colNum, curChar);
     }
 
 }
