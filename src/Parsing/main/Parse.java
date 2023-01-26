7cb7133bdf905a79dac8e4ae00aaf5297a9e7795
Kippiii <ianorzel@gmail.com>
2023-01-26 10:06:51 -0500

    Implementing first full draft of parser

--- src/Parsing/main/Parse.java
+++ src/Parsing/main/Parse.java
@@ -1,4 +1,21 @@
-package Parsing.main;
+package main;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import parser.*;
 
 public class Parse {
+    static String filename = "Factorial.java";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length >= 1)
+            filename = args[0];
+
+        final MiniJavaParser lexer = new MiniJavaParser (new FileInputStream(filename));
+        try {
+            lexer.Program();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
